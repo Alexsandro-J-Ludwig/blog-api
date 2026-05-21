be4c036192c282b1adf4ca56e7c4c379fdf7a54b
Alexsandro-J-Ludwig <alexsandrojacksomludwigvasco@gmail.com>
2026-05-21 16:05:52 -0300

    fix: change image columns to text to support base64 uploads

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -17,6 +17,7 @@ export class User {
   password: string;
 
   @Column({
+    type: 'text',
     default:
       'https://thumbs.dreamstime.com/b/default-avatar-profile-flat-icon-social-media-user-vector-portrait-unknown-human-image-default-avatar-profile-flat-icon-184330869.jpg',
   })
